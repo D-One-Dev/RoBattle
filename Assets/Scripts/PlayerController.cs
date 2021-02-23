71b9632a1ac94b7fd31e93f2f51cba90921ad4f9
MikeRuk05 <misharuk05@mail.ru>
2021-02-23 13:38:43 +0300

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float jumpForce;
+    private bool jump = false;
+    void Start()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0.1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-0.1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!jump)
+            {
+                rb.AddForce(new Vector2(0f, jumpForce));
+                jump = true;
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            jump = false;
+        }
+    }
+}
