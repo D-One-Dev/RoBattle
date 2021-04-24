33235f7761f8b1c35f5a1fd2c4495bdf835d2980
MikeRuk05 <misharuk05@mail.ru>
2021-04-25 01:20:20 +0300

    Added boss attack sprite

--- Assets/Scripts/BossAttackController.cs
+++ Assets/Scripts/BossAttackController.cs
@@ -5,29 +5,32 @@ using UnityEngine;
 public class BossAttackController : MonoBehaviour
 {
     [SerializeField] private float smooth;
-    private int platform = -1, stage = 0;
+    [SerializeField] private GameObject particles;
+    private int platform = -1, stage = 0, particlesStage;
     private float posX;
     private Vector3 move = Vector3.zero;
     void FixedUpdate()
     {
         if (transform.position.x != 0f && stage == 0) stage = 1;
-        else if (transform.position.y >= -9.001f && stage == 1) stage = 2;
-        else if (transform.position.y >= -2.001f && stage == 2) stage = 3;
+        else if (transform.position.y >= -10.501f && stage == 1) stage = 2;
+        else if (transform.position.y >= -6.001f && stage == 2) stage = 3;
         if (stage == 1)
         {
-            transform.localScale = new Vector3(2.9f, transform.localScale.y, transform.localScale.z);
-            move = Vector3.Lerp(transform.position, new Vector3(transform.position.x, -9f, transform.position.z), smooth * Time.fixedDeltaTime);
+            transform.localScale = new Vector3(6.4f, transform.localScale.y, transform.localScale.z);
+            move = Vector3.Lerp(transform.position, new Vector3(transform.position.x, -10.5f, transform.position.z), smooth * Time.fixedDeltaTime);
             transform.position = move;
+            PlayParticles();
         }
         else if (stage == 2)
         {
-            transform.localScale = new Vector3(3f, transform.localScale.y, transform.localScale.z);
-            move = Vector3.Lerp(transform.position, new Vector3(transform.position.x, -2f, transform.position.z), smooth * Time.fixedDeltaTime);
+            transform.localScale = new Vector3(6.5f, transform.localScale.y, transform.localScale.z);
+            move = Vector3.Lerp(transform.position, new Vector3(transform.position.x, -6f, transform.position.z), smooth * Time.fixedDeltaTime);
             transform.position = move;
+            PlayParticles();
         }
         else if (stage == 3)
         {
-            transform.localScale = new Vector3(2.9f, transform.localScale.y, transform.localScale.z);
+            transform.localScale = new Vector3(6.4f, transform.localScale.y, transform.localScale.z);
             move = Vector3.Lerp(transform.position, new Vector3(transform.position.x, -15f, transform.position.z), smooth * Time.fixedDeltaTime);
             transform.position = move;
         }
@@ -43,5 +46,14 @@ public class BossAttackController : MonoBehaviour
         else if (platform == 2) posX = 40f;
         transform.position = new Vector3(posX, -15f, transform.position.z);
         stage = 0;
+    }
+    private void PlayParticles()
+    {
+        if (stage != particlesStage)
+        {
+            particles.transform.position = new Vector3(transform.position.x, -5.28f, 0);
+            particles.GetComponent<ParticleSystem>().Play();
+            particlesStage = stage;
+        }
     }
 }
